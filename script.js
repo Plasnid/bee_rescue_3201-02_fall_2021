54a61d748cd3e18761e968277c2daf4c0438748e
Plasnid <stuart@sackler.ca>
2021-09-27 12:35:23 -0400

    adding notes regarding the symbols for the story

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+/**
+ * 
+ * * our symbols for our story are:
+ * * - turtle
+ * * - arrow
+ * * - keyhole
+ * * - key
+ * * - bee
+ * 
+ * * Thanks for being awesome.  Lets make something cool!
+ * 
+ * @param {*} beeName  - a string for the name of the bee
+ * @param {*} beeColour - a string for the colour of the bee
+ * @param {*} beeSize - a number for the size of the text showing the bee
+ */
+
 function describeBee(beeName, beeColour, beeSize){
     console.log(`%c ${beeName} is ${beeColour}`,`color:${beeColour};font-size:${beeSize}px`);
 };
